7416809640a327bbaec4e47a24499c4e33e8f589
Farhan Ramadhan <farcool17@gmail.com>
2023-12-08 16:02:43 +0700

    Ranking edit

--- ranking/views.py
+++ ranking/views.py
@@ -16,18 +16,18 @@ def rank_by_subscribers(request):  # pake filter contains
     graph = GraphLoader().graph
 
     query = prefix + """
-            SELECT DISTINCT ?uri ?rank ?title ?subscribers ?video_views ?channel_type ?created_date 
+            SELECT DISTINCT ?uri ?rank ?title ?subscribers ?subscribers_for_last_30_days ?channel_type ?created_date 
             WHERE {{
                 ?uri :Title ?title ;
                 :rank ?rank ;
                 :subscribers ?subscribers ;
                 :channel_type ?channel_type ;
-                :video_views ?video_views ;
+                :subscribers_for_last_30_days ?subscribers_for_last_30_days ;
                 :created_date ?created_date .
 
         }}
         ORDER BY desc(?subscribers)
-        LIMIT 200
+        LIMIT 100
         """
 
     qres = graph.query(query)
@@ -39,7 +39,7 @@ def rank_by_subscribers(request):  # pake filter contains
             "rank": row.rank,
             "subscribers": row.subscribers,
             "channel_type": row.channel_type,
-            "video_views": row.video_views,
+            "subscribers_for_last_30_days": row.subscribers_for_last_30_days,
             "created_date": row.created_date
         })
 
@@ -47,36 +47,22 @@ def rank_by_subscribers(request):  # pake filter contains
 
     return render(request, 'ranking.html', context)
 
-from django.shortcuts import render
-from django.http import HttpResponse
-from graph_loader import GraphLoader
-import sys
-from SPARQLWrapper import SPARQLWrapper, JSON
-
-# Create your views here.
-
-endpoint_url = "https://query.wikidata.org/sparql"
-prefix = """
-PREFIX : <http://localhost:8000/> 
-"""
-
-
 def rank_by_viewers(request):  # pake filter contains
     graph = GraphLoader().graph
 
     query = prefix + """
-            SELECT DISTINCT ?uri ?rank ?title ?subscribers ?video_views ?channel_type ?created_date 
+            SELECT DISTINCT ?uri ?rank ?title ?video_views ?video_views_for_the_last_30_days ?channel_type ?created_date 
             WHERE {{
                 ?uri :Title ?title ;
                 :rank ?rank ;
-                :subscribers ?subscribers ;
-                :channel_type ?channel_type ;
                 :video_views ?video_views ;
+                :channel_type ?channel_type ;
+                :video_views_for_the_last_30_days ?video_views_for_the_last_30_days ;
                 :created_date ?created_date .
 
         }}
         ORDER BY desc(?video_views)
-        LIMIT 200
+        LIMIT 100
         """
 
     qres = graph.query(query)
@@ -86,9 +72,9 @@ def rank_by_viewers(request):  # pake filter contains
             "uri": row.uri.split('/')[-1],
             "title": row.title,
             "rank": row.rank,
-            "subscribers": row.subscribers,
-            "channel_type": row.channel_type,
             "video_views": row.video_views,
+            "channel_type": row.channel_type,
+            "video_views_for_the_last_30_days": row.video_views_for_the_last_30_days,
             "created_date": row.created_date
         })
 
